ae269997685e9cd2c6e3ccdd8d976745b39a3c13
Ryan Kingsbury <ryan.kingsbury@gmail.com>
2015-01-04 22:21:58 -0500

    Added enumeration code, minor layout changes

--- app/src/main/java/com/example/kingryan/serialtest/MainActivity.java
+++ app/src/main/java/com/example/kingryan/serialtest/MainActivity.java
@@ -1,19 +1,53 @@
 package com.example.kingryan.serialtest;
 
+import android.content.Context;
+import android.hardware.usb.UsbManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private UsbManager mUsbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
     }
 
+    public void probeUsb(View v) {
+        // Handle button click
+        Log.i(TAG,"Starting probe");
+
+        final List<UsbSerialDriver> drivers =
+                UsbSerialProber.getDefaultProber().findAllDrivers(mUsbManager);
+
+        Log.i(TAG,"Number of drivers: "+Integer.toString(drivers.size()));
+
+        final List<UsbSerialPort> result = new ArrayList<UsbSerialPort>();
+        for (final UsbSerialDriver driver : drivers) {
+            final List<UsbSerialPort> ports = driver.getPorts();
+            Log.d(TAG, String.format("+ %s: %s port%s",
+                    driver, Integer.valueOf(ports.size()), ports.size() == 1 ? "" : "s"));
+            result.addAll(ports);
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +70,7 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }

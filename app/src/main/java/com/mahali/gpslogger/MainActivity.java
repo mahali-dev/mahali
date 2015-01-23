fb63a1bac2a9aad89d27b526463206c3b4de1031
Ryan Kingsbury <kingryan@mit.edu>
2015-01-23 17:30:29 -0500

    Stripped out old settings activity.

--- app/src/main/java/com/mahali/gpslogger/MainActivity.java
+++ app/src/main/java/com/mahali/gpslogger/MainActivity.java
@@ -319,7 +319,8 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+        //        return true;
     }
 
     @Override
@@ -331,8 +332,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent settingsIntent = new Intent(this, SettingsActivity.class);
-            startActivity(settingsIntent);
+//            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+//            startActivity(settingsIntent);
             return true;
         }
 

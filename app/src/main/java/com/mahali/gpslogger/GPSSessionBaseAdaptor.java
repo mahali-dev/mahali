c863ae1b264d23db0ec3753771604adb09c3c424
Ryan Kingsbury <kingryan@mit.edu>
2015-01-28 15:47:48 -0500

    Made session file sizes more readible. (e.g. '1.7 MB' instead of '1723844')

--- app/src/main/java/com/mahali/gpslogger/GPSSessionBaseAdaptor.java
+++ app/src/main/java/com/mahali/gpslogger/GPSSessionBaseAdaptor.java
@@ -54,9 +54,21 @@ public class GPSSessionBaseAdaptor extends BaseAdapter {
         }
 
         holder.txtSessionName.setText(sessionList.get(position).getFileName());
-        holder.txtSessionSize.setText(""+sessionList.get(position).getSize());
+
+        String s = humanReadableByteCount(sessionList.get(position).getSize(),true);
+        holder.txtSessionSize.setText(s);
 
         return convertView;
+    }
+
+    // Taken from here:
+    // http://stackoverflow.com/questions/3758606/how-to-convert-byte-size-into-human-readable-format-in-java
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 
     static class ViewHolder {
